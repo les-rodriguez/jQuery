a2a7aabc57d754b58a9c21d12fda4634fa251df7
les-rodriguez <lesli.rodriguezz98@gmail.com>
2023-08-27 01:45:31 -0600

    jquery practice

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.navItem').css("color", "white");
+
+    $('#nav').css("background-color", "#145");
+  
+    $('li:nth-child(even)').css("background-color", "#ccc");
+  
+    $('li:nth-child(odd)').css("background-color", "#aaa");
+  
+    $('input').val("Lesli");
+  });
